494730c7a9026df46cc0bf54c4da82e26b3560ac
Esteban Donis <estebandonis10@gmail.com>
2021-09-12 20:57:08 -0600

    Agregue los archivos Interaccion y principal

---
+++ interaccion.py
@@ -0,0 +1,34 @@
+##########################################
+#Interaccion
+#
+#Autor: Oscar Esteban Donis Martínez
+#Ultima modificación: 12/09/21
+#
+#Parte del programa que servira para
+#interactuar con el usuario
+##########################################
+
+def opciones_menu():
+    
+    
+def bienvenida():
+    
+    
+def despedida():
+    
+    
+def mal_dato():
+    
+    
+def valor_invalido():
+    
+    
+def nombre_donante():
+    
+    
+def ubicacion_donante():
+    
+    
+def nombre_ayudante():
+    
+    
