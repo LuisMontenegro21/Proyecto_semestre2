d0e666da0429527b23455d440b44593ea3ab369e
Samuel Argueta <arg211024@uvg.edu.gt>
2021-09-17 22:10:12 -0600

    Realizar cambios en donde sea requerido. Se ha iniciado con el código base para el programa.

--- Interaccion.java
+++ Interaccion.java
@@ -1,9 +1,96 @@
 import javax.swing.JOptionPane;
 
 /**
+ * Clase que funciona para la Interaccion con el usuario, siendo la Vista en la
+ * métodologia M-V-C
  * 
+ * @author Josúe Samuel Argueta Hernández.
+ * @since
+ * @version 1.0
  */
 
 public class Interaccion {
+    /**
+     * Método que muestra un mensaje de bienvenida al usuario.
+     */
+    public void bienvenida() {
+        System.out.println("BIENVENIDO AL SISTEMA DE DONACIONES");
+    }
 
+    /**
+     * Método que muestra un mensaje de "ERROR" en cuánto se ingresa un valor de
+     * tipo no numérico.
+     * 
+     * @return
+     */
+    public String mal_dato() {
+        String mal_dato = "DEBE INGRESAR UN VALOR QUE SEA NUMERICO";
+        return mal_dato;
+    }
+
+    /**
+     * Método que muestra un mensaje de "ERROR" en cuánto se ingresa un valor que no
+     * esta en las opcioens proveídas.
+     */
+    public String fuera_de_rango() {
+        String fuera_rango = "DEBE INGRESAR UN VALOR QUE SE ENCUENTRE EN LAS OPCIONES";
+        return fuera_rango;
+    }
+
+    /**
+     * Método que muestra un mensaje de despedida al usuario en cuanto deje el
+     * sistema.
+     */
+    public void despedida() {
+        System.out.println("MUCHAS GRACIAS POR SU INTERES!");
+    }
+
+    /**
+     * Método que muestra las opciones que estarán disponibles para la personas
+     */
+    private void menu_inicio() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+
+    /**
+     * Método que muestra las opciones correspondiente una vez se haya seleccionada
+     * una opcion principal.
+     */
+    private void menu_opciones() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+
+    /**
+     * Método que sirve para poder obtener un valor correcto por parte del usuario
+     * en base a las opciones proveidas.
+     * 
+     * @return seleccion: byte
+     */
+    public byte seleccion_menu_inicial() {
+        byte seleccion = 0;
+        boolean pasar = false;
+        do {
+            // cambiar este metodo al que corresponde con las opciones que se presentaran.
+            // cantidad_memoria();
+            try {
+                seleccion = Byte.parseByte(JOptionPane.showInputDialog("Ingrese la cantidad de memoria que desea"));
+                // Modificar el valor el cual se verifica que este en rango (1-5)
+                if ((seleccion < 1) || (seleccion > 5)) {
+                    System.out.println(fuera_de_rango());
+                } else {
+                    pasar = true;
+                }
+            } catch (NumberFormatException exception) {
+                System.err.println(mal_dato());
+            }
+        } while (!pasar);
+
+        return seleccion;
+    }
 }
--- Principal.java
+++ Principal.java
@@ -1,10 +1,33 @@
 /**
+ * Método que realiza las conexiones necesarias para poder obtener y devolver la
+ * inforamción necesario según sea el caso. Esta es la clase Controlador,
+ * siguiendo la metodología M-V-C
  * 
+ * @author
+ * @since
+ * @version 1.0
  */
 public class Principal {
-    private Interaccion vista = new Interaccion();
+    private static Interaccion vista = new Interaccion();
 
     public static void main(String[] args) {
-
+        vista.bienvenida();
+        byte opcion = 0;
+        // Modificar el valor según sea la modificación futura.
+        while (opcion != 3) {
+            opcion = vista.seleccion_menu_inicial();
+            // Agregar mas opciones segun se considere
+            switch (opcion) {
+                case 1:
+                    // ¿ROPA a donar?
+                    break;
+                case 2:
+                    // ¿COMIDA a donar?
+                    break;
+                default:
+                    // ¿OTRO donativos?
+                    break;
+            }
+        }
     }
 }
