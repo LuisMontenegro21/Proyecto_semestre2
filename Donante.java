900aebefc46512278233d46fd83eceea7c7938b6
Esteban Donis <estebandonis10@gmail.com>
2021-09-27 11:30:29 -0600

    Se creo la clase Donante

---
+++ Donante.java
@@ -0,0 +1,12 @@
+/**
+ * Método base que nos ayuda a crear a los donantes que se van a registrar en
+ * nuestra aplicacion.
+ * 
+ * @author Esteban Donis
+ * @since
+ * @version 1.0
+ */
+
+public class Donante {
+    
+}
